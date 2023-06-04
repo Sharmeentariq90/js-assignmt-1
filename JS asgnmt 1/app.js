6c729a8bc80d12de0ebb661bd8084b1f77e45494
Sharmeentariq90 <122076590+Sharmeentariq90@users.noreply.github.com>
2023-06-04 04:53:16 -1100

    Add files via upload

---
+++ JS asgnmt 1/app.js
@@ -0,0 +1,181 @@
+// // CHAPTER #1
+// var name="hello";
+// alert(name);
+// // Q no:1
+// var userName = "Dear";
+// alert("Welcome " + userName + "!");
+// // Q no:2
+// // alert("plz enter a valid password!");
+// var massage="PLz enter a valid password";
+// alert(massage);
+// // Q no:3
+// alert("Welcome to JS Land...\n Happy Coding!");
+// // Q no:4
+// var massage1="Welcome to JS Land...";
+// var massage2="Happy Coding!";
+// alert(massage1);
+// alert(massage2);
+// // Q no:5
+// var massage3 = ("Hello...I can run JS through my web browser's console");
+// console.log(massage3);
+// //////////////////////////xxxxxxxx/////////////////////////
+// // CHAPTER #2
+// // Q no:1
+// var username="sharmeen";
+// // Q no:2
+// var myname = prompt("your full name", '');
+// console.log(myname)
+// // Q no:3
+// var x="HELLO WORLD";
+// alert(x);
+// // Q no:4
+// var name="JOhn Doea";
+// var year="15 years old";
+// var qualification="Certified Mobile Application Development";
+// alert(name);
+// alert(year);
+// alert(qualification);
+// // Q no:5
+// var piz="pizza\npizz\npiz\npiz\npi\np";
+// alert(piz);
+// // Q no:6
+// var email=prompt("your Email adress plz",'xyz@gmail.com');
+// alert(email);
+// // Q no:7
+// var book="“A smarter way to learn JavaScript”.";
+// var massage="I m tring to learn from ";
+// alert(massage + book);
+// // Q no:8
+// document.write("Yeah...book.i m write HTML document on JAVASCRIPT");
+// // Q no:9
+// var sign="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(sign);
+// /////////////xxxxxxxxxxxxxxxx/////////////////////
+// // Q no 1:
+// var age="15 years old";
+// alert(age);
+// // Q no2:
+// var visit=14;
+// alert("you have visited "+14+"times");
+// // Q n0 3:
+// var Birthyear="my BIRTHYEAR is 1990";
+// document.write("MY birthday year is 1990");
+// // Q no 4:
+// var store="XYZclothing store";
+// var name="JOHN";
+// var qantity="5";
+// var product="T-shirt";
+// document.write(name +"ordered"+ quantity + product+"from"+store);
+// //////////////////////////xxxxxxxxxxxxxxxxx///////////////////////////////
+// // Q no:1
+// var x="hello";
+// var y="Whole";
+// var z="world"
+// // Q no:2:
+// // legal variable:
+// // 1._my variables
+// // 2.this_is_valid
+// // 3.a123
+// // 4.$money
+// // 5._underscore
+// // lillegal variable:
+// // 1.123illrgal
+// // 2.var
+// // 3.function
+// // 4.ture
+// // 5.null
+// // Q no:3
+// document.write("3. Display this in your browser");
+// ///////////////xxxxxxxxxxxxxxxxxxxxxxx/////////////////
+// // <!-- Q no1: -->
+// // var a=(5);
+// // var b=(3);
+// // alert(a+b);
+// // document.write("sum of 5+3 = 8 </br>");
+// // // Q no 2:
+// // var a=(5);
+// // var b=(3);
+// // alert(a-b);
+// // document.write("subtraction of 5-3 =" + (a-b)+"<br>");
+// // ////////////////////
+// // var a=(5);
+// // var b=(3);
+// // alert(a*b);
+// // document.write("Multiplication of a*b="+(a*b)+"<br>");
+// // alert(a|b);
+// // document.write("Modula of a|b is"+(a|b)+"<br>");
+// // Q no:3
+// // Declare a variable
+// // var num;
+
+// // // Show the value of the variable after declaration
+// // document.write("Value after variable declaration is: " + num + "<br>");
+
+// // // Initialize the variable with some number
+// // num = 5;
+
+// // // Show the initial value of the variable
+// // document.write("Initial value: " + num + "<br>");
+
+// // // Increment the variable
+// // num++;
+
+// // // Show the value of the variable after increment
+// // document.write("Value after increment is: " + num + "<br>");
+
+// // // Add 7 to the variable
+// // num += 7;
+
+// // // Show the value of the variable after addition
+// // document.write("Value after addition is: " + num + "<br>");
+
+// // // Decrement the variable
+// // num--;
+
+// // // Show the value of the variable after decrement
+// // document.write("Value after decrement is: " + num + "<br>");
+
+// // // Show the remainder after dividing the variable's value by 3
+// // var remainder = num % 3;
+// // document.write("The remainder is: " + remainder);
+// // Q no:4
+// // let PTicket=(600);
+// // let CostOfBuying=(5*PTicket);
+// // document.write("Total cost to buying 5 tickets is " + CostOfBuying );
+// // Q no:5;
+// // let num=+prompt("write a table you want to generate");
+// // document.write("<h3>Table of "+ num +"</h3>");
+// // for(let i=1;i<=10;i++)
+// // {document.write(num+"x"+i+"="+num*i+"<br>");}
+// // Q no:6
+// // (a)
+// // let c=25;
+// // let f=((c*9/5)+32);
+// // document.write(c+"C°"+" is "+ f+"F° </br>");
+// // // (b)
+// // let F=70;
+// // let C=(F - 32) * 5/9;
+// // document.write(F+"F° is "+C+"C°");
+// // Q no 7:\
+// // store the price and quantity of each item in variables
+// const item1Price = 10;
+// const item2Price = 20;
+// const item1Qty = 2;
+// const item2Qty = 1;
+// const shippingCharges = 5;
+
+// // compute the total cost
+// const subtotal = item1Price * item1Qty + item2Price * item2Qty;
+// const total = subtotal + shippingCharges;
+
+// // display the receipt in the browser
+// document.write("<h2>Receipt</h2>");
+// document.write("<p>Item 1 Price: $" + item1Price + "</p>");
+// document.write("<p>Item 1 Quantity: " + item1Qty + "</p>");
+// document.write("<p>Item 2 Price: $" + item2Price + "</p>");
+// document.write("<p>Item 2 Quantity: " + item2Qty + "</p>");
+// document.write("<p>Subtotal: $" + subtotal + "</p>");
+// document.write("<p>Shipping Charges: $" + shippingCharges + "</p>");
+// document.write("<p><strong>Total: $" + total + "</strong></p>");
+
+
